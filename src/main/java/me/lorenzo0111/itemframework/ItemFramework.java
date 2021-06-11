9d97dd8f36bc20c56dc71fe15a387c4e87081373
Lorenzo <69311874+Lorenzo0111@users.noreply.github.com>
2021-06-11 16:38:08 +0200

    Added License and listener

--- src/main/java/me/lorenzo0111/itemframework/ItemFramework.java
+++ src/main/java/me/lorenzo0111/itemframework/ItemFramework.java
@@ -1,7 +1,33 @@
+/*
+ * This file is part of RocketPlaceholders, licensed under the MIT License.
+ *
+ * Copyright (c) Lorenzo0111
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package me.lorenzo0111.itemframework;
 
 import me.lorenzo0111.itemframework.items.CustomItem;
+import me.lorenzo0111.itemframework.listener.ClickListener;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
@@ -51,6 +77,14 @@ public final class ItemFramework {
         CustomItem item = new CustomItem(id);
         STORAGE.put(id,item);
         return item;
+    }
+
+    /**
+     * Required to make the framework work
+     * @param plugin Plugin that is using the framework
+     */
+    public static void init(JavaPlugin plugin) {
+        new ClickListener(plugin);
     }
 
     /**
